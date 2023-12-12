af6d9b946c7ce51e174d3dada2493d1c370fea84
aph5nt <dorotawojciechowska85@gmail.com>
2023-12-12 22:03:35 +0100

    added pydantic to protocol.py

--- insights/protocol.py
+++ insights/protocol.py
@@ -1,10 +1,9 @@
 from typing import Optional, List, Dict
 import bittensor as bt
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 # Model types
 MODEL_TYPE_FUNDS_FLOW = "funds_flow"
-MODEL_TYPE_FUNDS_FLOW_V1 = "funds_flow-v1.0"
 
 # Networks
 NETWORK_BITCOIN = "bitcoin"
@@ -14,23 +13,22 @@ NETWORK_DASH = "dash"
 NETWORK_ZCASH = "zcash"
 NETWORK_BITCOIN_CASH = "bitcoin_cash"
 
+NETWORKS = [ NETWORK_BITCOIN, NETWORK_LITECOIN, NETWORK_DOGE, NETWORK_DASH, NETWORK_ZCASH, NETWORK_BITCOIN_CASH ]
+MODELS = [ MODEL_TYPE_FUNDS_FLOW ]
 
 class MinerDiscoveryMetadata(BaseModel):
-    network: str = None
-    model_type: str = None
-    graph_schema: Optional[Dict] = None
-    #TODO: implement method for getting graph schema from miner
-
+    network: str = Field(..., description="Network type", enum=NETWORKS)
+    model_type: str = Field(..., description="Model type", enum=MODELS)
+    graph_schema: Optional[Dict] = Field(None, description="Graph schema")
 
 class MinerDiscoveryOutput(BaseModel):
-    metadata: MinerDiscoveryMetadata = None
-    data_samples: List[Dict] = None
-    block_height: int = None
-    start_block_height: int = None
-
+    metadata: MinerDiscoveryMetadata = Field(..., description="Metadata about the miner discovery")
+    data_samples: List[Dict] = Field(..., description="List of data samples")
+    block_height: int = Field(..., description="Block height")
+    start_block_height: int = Field(..., description="Start block height")
 
 class MinerDiscovery(bt.Synapse):
-    output: MinerDiscoveryOutput = None
+    output: MinerDiscoveryOutput = Field(..., description="Output of the miner discovery")
 
     def deserialize(self):
         return self
@@ -43,4 +41,15 @@ class MinerQuery(bt.Synapse):
     output: Optional[List[Dict]] = None
 
     def deserialize(self) -> List[Dict]:
+        return self.output
+
+
+class MinerQuery(bt.Synapse):
+    network: str = Field(..., description="Network type", enum=NETWORKS)
+    model_type: str = Field(..., description="Model type", enum=MODELS)
+    query: str = Field(..., description="Cypher Query")
+    output: Optional[List[Dict]] = Field(None, description="Query output")
+
+    def deserialize(self) -> List[Dict]:
+        # Implement deserialization logic if necessary
         return self.output
