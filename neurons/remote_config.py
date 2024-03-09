3175f64f2102d70cfdb96047d06f3ae7fa41aa24
Kamil Wojciechowski <camdorasoftware@protonmail.com>
2024-03-09 09:59:57 +0000

    set grace_threshold_score

--- neurons/remote_config.py
+++ neurons/remote_config.py
@@ -131,7 +131,7 @@ class ValidatorConfig(RemoteConfig):
         self.is_grace_period = None
         
         ## Added grace_threshold_block
-        self.grace_threshold_block = 1e+5
+        self.grace_threshold_score = 0.428844
         
         
         self.config_url = os.getenv("VALIDATOR_REMOTE_CONFIG_URL", 'https://subnet-15-cfg.s3.fr-par.scw.cloud/validator3.json')
@@ -152,7 +152,7 @@ class ValidatorConfig(RemoteConfig):
         self.blockchain_recency_weight = self.get_config_value('blockchain_recency_weight',  {"bitcoin": 2, "doge": 2})
         self.is_grace_period = self.get_config_value('is_grace_period', False)
         
-        self.grace_threshold_block = self.get_config_value('grace_threshold_block', 1e+5)
+        self.grace_threshold_score = self.get_config_value('grace_threshold_score', 0.428844)
         return self
 
     def get_blockchain_min_blocks(self, network):
