10c07a9788f5d29366fa3e91ad805a2f7b24d3ea
kamil <kamil.wojciechowski@chain-insights.ai>
2024-05-19 10:48:44 +0200

    added neuron module range default json logging

---
+++ neurons/__init__.py
@@ -0,0 +1,36 @@
+import json
+
+import bittensor as bt
+
+
+def serialize(record):
+    tmstamp = format(record['time'], "%Y-%m-%d %H:%M:%S.%03d")
+    subset = {
+        'timestamp': tmstamp,
+        'level': record['level'].name,
+        'message': record['message'],
+    }
+    subset.update(record['extra'])
+    return json.dumps(subset)
+
+
+def patching(record):
+    record['message'] = serialize(record)
+
+
+def custom_log_formatter(record):
+    """Custom log formatter"""
+    return "{message}\n"
+
+
+bt.btlogging.logging.log_formatter = custom_log_formatter
+bt.btlogging.logger = bt.btlogging.logger.patch(patching)
+bt.btlogging.logging.debug = bt.btlogging.logger.debug
+bt.btlogging.logging.info = bt.btlogging.logger.info
+bt.btlogging.logging.warning = bt.btlogging.logger.warning
+bt.btlogging.logging.error = bt.btlogging.logger.error
+
+
+
+
+
