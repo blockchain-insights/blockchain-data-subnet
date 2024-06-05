8e51520891d07f98a7bc91d020caa584c98ede40
renesweet24 <renesweet0204@gmail.com>
2024-06-04 21:11:58 -0500

    fix: torch normalization to numpy

--- template/base/validator.py
+++ template/base/validator.py
@@ -232,7 +232,8 @@ class BaseValidatorNeuron(BaseNeuron):
 
         # Calculate the average reward for each uid across non-zero values.
         # Replace any NaN values with 0.
-        raw_weights = np.linalg.norm(self.scores, ord=1, axis=0, keepdims=True)
+        norm = np.linalg.norm(self.scores, ord=1, axis=-1, keepdims=True)
+        raw_weights = self.scores / norm
 
         # Process the raw weights to final_weights via subtensor limitations.
         (
