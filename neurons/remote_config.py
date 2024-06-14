574cf1a5404fbe5e263f730a1b50dd7174e1b185
renesweet24 <renesweet0204@gmail.com>
2024-06-14 14:09:11 -0500

    fix: indentation error with alternate code

--- neurons/remote_config.py
+++ neurons/remote_config.py
@@ -216,21 +216,17 @@ class ValidatorConfig(RemoteConfig):
         return self.get_config_composite_value(f'blockchain_recency_weight.{network}', 2)
 
     def get_benchmark_cypher_query_script(self, network):
-        return self.get_config_composite_value(f'benchmark_cypher_query_script.{network}', """
-            import random
-            def build_query(network, start_block, end_block, diff = 10000):
-                mid_point = start_block + (end_block - start_block) // 2
-                block_num = random.randint(mid_point, end_block - diff)
-                return f"UNWIND range({block_num}, {block_num + diff}) AS block_height MATCH (p:Transaction) WHERE p.block_height = block_height RETURN SUM(p.block_height);"
-            query = build_query(network, start_block, end_block)
-            """)
+        return self.get_config_composite_value(f'benchmark_cypher_query_script.{network}', """import random
+def build_query(network, start_block, end_block, diff = 10000):
+    mid_point = start_block + (end_block - start_block) // 2
+    block_num = random.randint(mid_point, end_block - diff)
+    return f"UNWIND range({block_num}, {block_num + diff}) AS block_height MATCH (p:Transaction) WHERE p.block_height = block_height RETURN SUM(p.block_height);"
+query = build_query(network, start_block, end_block)""")
 
     def get_benchmark_sql_query_script(self, network):
-        return self.get_config_composite_value(f'benchmark_sql_query_script.{network}', """
-            import random
-            def build_query(network, start_block, end_block, diff = 10000):
-                mid_point = start_block + (end_block - start_block) // 2
-                block_num = random.randint(mid_point, end_block - diff)
-                return f"SELECT * from balance_changes WHERE block BETWEEN '{block_num}' AND '{block_num + diff}';"
-            query = build_query(network, start_block, end_block)
-            """)
+        return self.get_config_composite_value(f'benchmark_sql_query_script.{network}', """import random
+def build_query(network, start_block, end_block, diff = 10000):
+    mid_point = start_block + (end_block - start_block) // 2
+    block_num = random.randint(mid_point, end_block - diff)
+    return f"SELECT sum(block) FROM balance_changes WHERE block BETWEEN {block_num} AND {block_num + diff}"
+query = build_query(network, 1, balance_end)""")
